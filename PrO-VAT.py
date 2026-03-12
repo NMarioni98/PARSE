5f81c2b4aff495d0752bc9ddacaeb75d6ecf7444
Nico Marioni <nico.marioni@hotmail.com>
2026-03-12 11:29:36 -0400

    Added print statements for when SA no calculated

--- PrO-VAT.py
+++ PrO-VAT.py
@@ -536,6 +536,11 @@ def volume_analysis(frame):
                         print('X {:10.5f} {:10.5f} {:10.5f} {:10.5f}'.format(sph[0] - cell[0]/2, sph[1] - cell[1]/2, sph[2] - cell[2]/2, args.L_voxel/2), file=anaout)
             print('Free volume surface xyz file printed\n')
     else:
+        print('Surface area calculation not performed.')
+        if args.Voxel_dist != 'Uniform':
+            print("    Set --Voxel_dist == 'Uniform'")
+        if cycle != int(np.ceil(1/args.rand_frac)):
+            print("    Set --tol == -1")
         SA = 0
     
 
@@ -926,5 +931,6 @@ if __name__ == "__main__":
     print('\n########################################')
     print(  '########################################')
     print(  '########################################\n')
+
 
     main()
